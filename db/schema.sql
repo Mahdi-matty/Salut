407ba69ade068037a12fbe8d6b5c180fff393fe6
Ryan Persaud <ryanpersaud811@gmail.com>
2023-11-23 21:10:09 -0500

    added models

--- db/schema.sql
+++
@@ -1,2 +0,0 @@
-DROP DATABASE IF EXISTS salut_db;
-CREATE DATABASE salut_db;
---
+++ models/User.js
@@ -0,0 +1,63 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.VARCHAR(30),
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.VARCHAR(30),
+      allowNull: false,
+      validate: {
+        len: [6],
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    following_user_id: {
+      type: DataTypes.INTEGER,
+    },
+    followed_user_id: {
+      type: DataTypes.INTEGER,
+    },
+    created_at: {
+      type: DataTypes.TIMESTAMP,
+    },
+  },
+  {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
